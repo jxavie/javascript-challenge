8a189d9e93d88b2bd21375a37d7c54055b8c3f84
jxavie <54754878+jxavie@users.noreply.github.com>
2020-01-11 22:25:15 -0500

    updating files

--- Part-1/02-HW_Form_Filter/static/js/app.js
+++ Part-1/02-HW_Form_Filter/static/js/app.js
@@ -5,7 +5,18 @@ var people = data;
 var button = d3.select("#button");
 
 // Complete the click handler for the form
-button.on("click", function() {
+button.on("click", onclick);
+
+// button.on("click", function() {
+function onclick(){
+
+  // Clear previous results
+  console.clear();
+  d3.select("#age_stats").html("");
+  d3.select("ul").html("");
+  d3.select("#filtered_table").html("");
+  d3.select("thead").html("");
+  d3.select("tbody").html("");
 
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#patient-form-input");
@@ -20,23 +31,26 @@ button.on("click", function() {
 
   // BONUS: Calculate summary statistics for the age field of the filtered data
 
-  // First, create an array with just the age values
+  // Create an array with just the age values
   var ages = [];
   
-  results = data.filter(bloodtype);
+  results = people.filter(bloodtype);
 
   results.forEach(function(result){
     ages.push(result.age)
   });
 
-  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-  mean_age = math.mean(ages);
-  median_age = math.median(ages);
-  mode_age = math.mode(ages);
-  variance_age = math.var(ages);
-  std_age = math.std(ages);
+  // Use math.js to calculate the mean, median, mode, var, and std of the ages
+  var mean_age = math.mean(ages);
+  var median_age = math.median(ages);
+  var mode_age = math.mode(ages);
+  var variance_age = math.var(ages);
+  var std_age = math.std(ages);
 
-  // Finally, add the summary stats to the `ul` tag
+  // Add age summary stats to the `ul` tag
+  var header_stat = d3.select("#age_stats");
+  header_stat.append("h4").text("Age Statistics")
+  
   var ul = d3.select("ul");
   ul.append("li").text(`Mean: ${mean_age}`);
   ul.append("li").text(`Median: ${median_age}`);
@@ -44,4 +58,39 @@ button.on("click", function() {
   ul.append("li").text(`Variance: ${variance_age}`);
   ul.append("li").text(`Standard Deviation: ${std_age}`);
 
+  // Display filtered data as a table
+  var table = d3.select("table");
+  table.attr("class", "table table-striped");
+
+  var header_table = d3.select("#filtered_table");
+  header_table.append("h4").text("Filtered Data Table")
+
+  var thead = d3.select("thead");
+  var th_row = thead.append("tr");
+  var labels = Object.keys(people[0]);
+  console.log(labels);
+  
+  labels.forEach(function(header){
+    theader = th_row.append("th").text(header);
+  })
+
+  var tbody = "";
+  tbody = d3.select("tbody");
+
+  results.forEach(function(result){
+    tb_row = "";
+    tb_row = tbody.append("tr");
+    td1 = tb_row.append("td").text(result.fullName);
+    td2 = tb_row.append("td").text(result.age);
+    td3 = tb_row.append("td").text(result.bloodType);
+  })
+};
+
+// Retrieve entry in case 'enter' is pressed
+var inputElement = document.getElementById("patient-form-input");
+inputElement.addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("button").click();
+  }
 });
--- Part-2/static/js/app.js
+++ Part-2/static/js/app.js
@@ -1,23 +1,252 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// create array of table headers
+var headers = Object.keys(tableData[0]);
+console.log(headers);
+
+//  create arrays of unique values for keys used for filtering
+var dates = [];
+var cities = [];
+var states = [];
+var countries = [];
+var shapes = [];
+
+tableData.forEach(function(obj){
+    dates.push(obj.datetime);
+    cities.push(obj.city);
+    states.push(obj.state);
+    countries.push(obj.country);
+    shapes.push(obj.shape);
+})
+console.log(dates);
+console.log(cities);
+console.log(states);
+console.log(countries);
+console.log(shapes);
+
+function uniqueValues(value, index, self) {
+    return self.indexOf(value) === index;
+};
+
+var dateList = dates.filter(uniqueValues);
+var cityList = cities.filter(uniqueValues);
+var stateList = states.filter(uniqueValues);
+var countryList = countries.filter(uniqueValues);
+var shapeList = shapes.filter(uniqueValues);
+
+// var dateList = [];
+// var cityList = [];
+// var stateList = [];
+// var countryList = [];
+// var shapeList = [];
+
+// for (var i=0; i<tableData.length; i++){
+//     if (i==0){
+//         dateList.push(dates[i]);
+//         cityList.push(cities[i]);
+//         stateList.push(states[i]);
+//         countryList.push(countries[i]);
+//         shapeList.push(shapes[i]);
+//     }
+//     else {
+//         dateList.forEach(function(j){
+//             if (j!=dates[i]){
+//                 dateList.push(dates[i]);
+//             };
+//         });
+//         cityList.forEach(function(j){
+//             if (j!=cities[i]){
+//                 cityList.push(cities[i]);
+//             };
+//         });
+//         stateList.forEach(function(j){
+//             if (j!=states[i]){
+//                 stateList.push(states[i]);
+//             };
+//         });
+//         countryList.forEach(function(j){
+//             if (j!=countries[i]){
+//                 countryList.push(countries[i]);
+//             };
+//         });
+//         shapeList.forEach(function(j){
+//             if (j!=shapes[i]){
+//                 shapeList.push(shapes[i]);
+//             };
+//         });
+//     }
+// };
+
+console.log(dateList);
+console.log(cityList);
+console.log(stateList);
+console.log(countryList);
+console.log(shapeList);
+
+// create options for each filter dropdown
+drop_date = d3.select("#date");
+dateList.forEach(function(val){
+    option = drop_date.append("option");
+    option.text(val);
+    option.attr("value",val);
+});
+drop_city = d3.select("#city");
+cityList.forEach(function(val){
+    option = drop_city.append("option");
+    option.text(val);
+    option.attr("value",val);
+});
+drop_state = d3.select("#state");
+stateList.forEach(function(val){
+    option = drop_state.append("option");
+    option.text(val);
+    option.attr("value",val);
+});
+drop_country = d3.select("#country");
+countryList.forEach(function(val){
+    option = drop_country.append("option");
+    option.text(val);
+    option.attr("value",val);
+});
+drop_shape = d3.select("#shape");
+shapeList.forEach(function(val){
+    option = drop_shape.append("option");
+    option.text(val);
+    option.attr("value",val);
+});
+
+// create options for each filter dropdown (alternate)
+// var options = drop_date.selectAll("option")
+//                     .data(dateList)
+//                     .enter()
+//                     .append("option")
+// options.text(function(d){return d;})
+//        .attr("value",function(d){return d;});
+
 var button = d3.select("#filter-btn");
 
+var thead = d3.select("thead");
 var tbody = d3.select("tbody");
 
-button.on("click", function(){
-    
-    var inputElement = d3.select("#datetime");
-    var inputValue = Date.parse(inputElement.property("value"));
-    console.log(inputValue);
+var trow = thead.append("tr");
+headers.forEach(function(header){
+    trow.append('th').text(header);
+});
 
-    function filter_date(record) {
-        return Date.parse(record.datetime) == inputValue;
+tableData.forEach(function(record){
+    trow = tbody.append("tr");
+    trow.append("td").text(record.datetime);
+    trow.append("td").text(record.city);
+    trow.append("td").text(record.state);
+    trow.append("td").text(record.country);
+    trow.append("td").text(record.shape);
+    trow.append("td").text(record.durationMinutes);
+    trow.append("td").text(record.comments);
+});
+
+button.on("click", sighting);
+
+function sighting(){
+
+    d3.select("thead").html("");
+    d3.select("tbody").html("");
+
+    var input_date = d3.select("#date").property("value");
+    var date = Date.parse(input_date);
+    console.log(date);
+
+    var city = d3.select("#city").property("value");
+    console.log(city);
+
+    var state = d3.select("#state").property("value");
+    console.log(state);
+
+    var country = d3.select("#country").property("value");
+    console.log(country);
+
+    var shape = d3.select("#shape").property("value");
+    console.log(shape);
+
+    if (input_date == "date") {
+        function filter_date(record) {
+            return record.datetime;
+            // return record.datetime == date;
+        };   
     }
+    else {
+        function filter_date(record) {
+            return Date.parse(record.datetime) == date;
+            // return record.datetime == date;
+        };   
+    };
+    
+    if (city == "city") {
+        function filter_city(record) {
+            return record.city;
+        };
+    }
+    else {
+        function filter_city(record) {
+            return record.city == city;
+        };
+    };
 
-    var results = tableData.filter(filter_date);
+    if (state == "state") {
+        function filter_state(record) {
+            return record.state;
+        };
+    }
+    else {
+        function filter_state(record) {
+            return record.state == state;
+        };
+    };
+
+    if(country == "country") {
+        function filter_country(record) {
+            return record.country;
+        };
+    }
+    else {
+        function filter_country(record) {
+            return record.country == country;
+        };
+    };
+
+    if (shape == "shape") {
+        function filter_shape(record) {
+            return record.shape;
+        };
+    }
+    else {
+        function filter_shape(record) {
+            return record.shape == shape;
+        };
+    };
+
+    var results = [];
+    if (input_date == "select date" &
+        city == "select city" &
+        state == "select state" &
+        country == "select country" &
+        shape == "select shape")
+        {
+            results = tableData;
+        }
+    else {
+        results = tableData.filter(filter_date)
+        .filter(filter_city)
+        .filter(filter_state)
+        .filter(filter_country)
+        .filter(filter_shape);
+    }
     console.log(results);
+
+    var trow = thead.append("tr");
+    headers.forEach(function(header){
+        trow.append('th').text(header);
+    });
 
     results.forEach(function(result){
         trow = tbody.append("tr");
@@ -28,6 +257,8 @@ button.on("click", function(){
         trow.append("td").text(result.shape);
         trow.append("td").text(result.durationMinutes);
         trow.append("td").text(result.comments);
-    })
-})
+    });
+
+    // d3.select("table").attr(class)
+};
 
